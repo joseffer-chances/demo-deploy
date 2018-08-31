75e213188eae5ada3180ba4238ded5478ed2469f
joseffer-chances <tvfilmes2015@gmail.com>
2018-08-31 16:08:12 -0300

    atualizarang6

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA-fIYV70j9IjH0w4ZzAcyw6ayy_R3J3YI",
+    authDomain: "ang6-demos-deploy.firebaseapp.com",
+    databaseURL: "https://ang6-demos-deploy.firebaseio.com",
+    projectId: "ang6-demos-deploy",
+    storageBucket: "ang6-demos-deploy.appspot.com",
+    messagingSenderId: "751048881099"
+  }
 };
