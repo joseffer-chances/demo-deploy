27414fd1abb3072cf6cb6f5efb539413663377cd
joseffer-chances <tvfilmes2015@gmail.com>
2018-08-31 15:57:11 -0300

    atualizacaoAng6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA-fIYV70j9IjH0w4ZzAcyw6ayy_R3J3YI",
+    authDomain: "ang6-demos-deploy.firebaseapp.com",
+    databaseURL: "https://ang6-demos-deploy.firebaseio.com",
+    projectId: "ang6-demos-deploy",
+    storageBucket: "ang6-demos-deploy.appspot.com",
+    messagingSenderId: "751048881099"
+  }
 };
 
 /*
